9977d19cda6fa0f8eac49827d190b4091cb8783f
Alexander Kondra and Evan Hadfield <evan@>
2019-03-25 13:37:58 -0700

    add bmi calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-var answer = alert(prompt("name") + " " + prompt("age") + " " + prompt("food") + ".");
+//var answer = alert(prompt("name") + " " + prompt("age") + " " + prompt("food") + ".");
+
+var add = function(number1, number2) {
+  return number1 / (number2 * number2);
+};
+
+var number1 = parseInt(prompt("Enter kG:"));
+var number2 = parseInt(prompt("Enter cm:"));
+
+var result = add(number1, number2);
+alert(result);
