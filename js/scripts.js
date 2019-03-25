461ee556a52478729d669601680d3b6f679be51e
Alexander Kondra and Evan Hadfield <evan@>
2019-03-25 14:08:41 -0700

    add converter celcious and farentheit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //var answer = alert(prompt("name") + " " + prompt("age") + " " + prompt("food") + ".");
 
-var add = function(number1, number2) {
+/*var add = function(number1, number2) {
   return number1 / (number2 * number2);
 };
 
@@ -8,4 +8,12 @@ var number1 = parseInt(prompt("Enter kG:"));
 var number2 = parseInt(prompt("Enter cm:"));
 
 var result = add(number1, number2);
-alert(result);
+alert(result);*/
+
+var farhenhiet = function(celcius) {
+	return (celcius * 9 / 5) + 32;
+};
+
+var cel = prompt("Enter Celcuios");
+
+alert(farhenhiet(cel));
